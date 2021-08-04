25612467c9ba479ac8b8048565bbd6a99f8f806e
Richard Dobroň <office.etexweb@gmail.com>
2021-08-04 17:40:13 +0200

    :sparkles: Disable entrypoint checkup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\CheckAppEntrypoint::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 

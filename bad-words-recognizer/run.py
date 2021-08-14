d5e6fb3d0dc3f1773b4138d1288ee4ed411e491a
RandoS-svk <47712449+RandoS-svk@users.noreply.github.com>
2021-08-14 12:10:42 +0200

    🎉 Improved AI for recognize bad words

---
+++ bad-words-recognizer/run.py
@@ -0,0 +1,10 @@
+import time
+
+restart = True
+while restart:
+    restart = False
+    python event-description.py
+    python event-name.py
+    python user-bio.py
+    time.sleep(1)
+    restart = True

bdd6d3d6d9f920150b88a4425174d6b65e1a7719
RandoS-svk <47712449+RandoS-svk@users.noreply.github.com>
2021-08-14 20:40:48 +0200

    🎉 Improvement text censure

---
+++ bad-words-recognizer/event-name-local.py
@@ -0,0 +1,52 @@
+import mysql.connector
+import smtplib
+from better_profanity import profanity
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import time
+
+mydb = mysql.connector.connect(host='localhost',database='hajduk_app',user='hajduk',password='vHgpUvwR*,;uS>')
+
+fromaddr = "no-reply@sportm4te.com"
+toaddr = "reports@sportm4te.com"
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "New text was censured"
+
+mycursor = mydb.cursor()
+
+a = 28
+
+should_restart = True
+while should_restart:
+    should_restart: False
+    a+=1
+    sql = f'SELECT name FROM event WHERE id = {a}'
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    if not myresult:
+        quit()
+    else:
+        for x in myresult:
+            non_censured = x[0]
+            profanity.load_censor_words(whitelist_words=['nad'])
+            profanity.load_censor_words_from_file('bad-words.txt')
+            censored_text = profanity.censor(x[0])
+            if non_censured != censored_text:
+                body = f'Non censured text: {non_censured}. Censured text: {censored_text}'
+                msg.attach(MIMEText(body, 'plain'))
+                server = smtplib.SMTP('smtps.platon.sk', 587)
+                server.ehlo()
+                server.starttls()
+                server.ehlo()
+                server.login('no-reply@sportm4te.com', '4Xv!h?t<#{mTvK')
+                text = msg.as_string()
+                server.sendmail(fromaddr, toaddr, text)
+                mycursor = mydb.cursor()
+                sql = f"UPDATE event SET name = '{censored_text}' WHERE id = '{a}'"
+                mycursor.execute(sql)
+                mydb.commit()
+                should_restart = True
+            else:
+                should_restart = True
